a62604deca0ed64080e6d5027d7ab8d95763bf36
Haythem Ben Salah <haythem.bensalah@softt365.com>
2022-12-09 10:37:56 +0100

    nexus

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def buildImage() {
 
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
-    sh 'mvn clean deploy -Dmaven.test.skip=true'
+    sh 'mvn clean deploy `-Dmaven.test.skip=true'
 }
 
 def sonarScan(String serverIp, String serverUser) {
